1b6365cdbbae17dd587a063867a7c6c68e206958
sme777 <samson.petrosyan8@gmail.com>
2022-02-06 14:46:52 -0800

    initial commit

---
+++ pop/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />  
+    </div>
+  )
+}
+
+export default Home
